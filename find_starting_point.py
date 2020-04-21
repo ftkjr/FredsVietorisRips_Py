29f9023a8230a2b23a8d270b8361c6008819aaaa
Fred <46111287+ftkjr@users.noreply.github.com>
2020-04-21 09:08:19 -0400

    First upload of a finished BDSCAN
    I mean, it sort of works, for the most part. Needs more testing.

--- find_starting_point.py
+++ find_starting_point.py
@@ -1,9 +1,11 @@
-def find_starting_point(adjacency_matrix, untraversed_points, minimum_connections):
-    starting_point = 0  
-    while sum(adjacency_matrix[starting_point]) < minimum_connections:
-        starting_point += 1
-        if starting_point == len(adjacency_matrix):
-            raise Exception('No point of the {} given met the minimum connection requirement. Consider reducing your minimum connections and try again.'.format(len(adjacency_matrix)))
+def find_starting_point(adjacency_matrix, untraversed_points, minimum_connections, talkative = False):
+    for starting_point in untraversed_points:
+        if sum(adjacency_matrix[starting_point]) >= minimum_connections:
+            untraversed_points.remove(starting_point)
 
-    untraversed_points.remove(starting_point)
-    return starting_point, untraversed_points
+            return starting_point, untraversed_points
+
+    else:
+        
+        return None, untraversed_points
+    
